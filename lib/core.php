4a7a5461bfb8c448f1909f1c8e2a08fbbac4cd32
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2011-03-18 10:08:14 +0100

    Added a system for pluggable model backends

--- lib/core.php
+++ lib/core.php
@@ -336,6 +336,19 @@ function sp_route_encode_params(array $params) {
 }
 
 /**
+ * Load a model and the backend for that model.
+ * @param string $module
+ * @param string $model
+ */
+function sp_model_load($module, $model) {
+    sp_module_require($module, "model/{$model}.php");
+    
+    if ($backend = _sp_model_backend($module, $model)) {
+        sp_module_require($module, "model/backend/{$backend}.php");
+    }
+}
+
+/**
  * Render a view script and get the output.
  * @param string $view
  * @param array $params
@@ -518,4 +531,25 @@ function _sp_view_include_helpers($module) {
     if (isset ($modules[$module]['view_helpers'])) {
         sp_require($modules[$module]['view_helpers']);
     }
+}
+
+/**
+ * Get the name of the backend for a model.
+ * @param string $module
+ * @param string $model
+ * @return string|null
+ */
+function _sp_model_backend($module, $model) {
+    $config = sp_config();
+    if (!isset ($config['backends'])) {
+        return null;
+    }
+    $backends = $config['backends'];
+    if (isset ($backends[$module][$model])) {
+        return $backends[$module][$model];
+    }
+    if (isset ($backends[$module]['default'])) {
+        return $backends[$module]['default'];
+    }
+    return null;
 }
